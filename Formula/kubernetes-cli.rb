6822e1b06804f99e34720ab069ecaa76e10ce47f
svacek <50051279+svacek@users.noreply.github.com>
2020-06-23 10:41:25 +0200

    Update kubernetes-cli.rb

--- Formula/kubernetes-cli.rb
+++ Formula/kubernetes-cli.rb
@@ -2,8 +2,8 @@ class KubernetesCli < Formula
   desc "Kubernetes command-line interface"
   homepage "https://kubernetes.io/"
   url "https://github.com/kubernetes/kubernetes.git",
-      :tag      => "v1.18.4",
-      :revision => "c96aede7b5205121079932896c4ad89bb93260af"
+      :tag      => "v1.17.2",
+      :revision => "59603c6e503c87169aea6106f57b9f242f64df89"
   head "https://github.com/kubernetes/kubernetes.git"
 
   bottle do
